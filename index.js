19f2767699d85dc11e3d6c015dab42320a6e4c40
KyleHS <k15156@yahoo.com>
2023-08-28 13:00:10 -0600

    connected application to mongoose

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true })
+    .then(() => {
+        console.log("mongo connection open")
+    })
+    .catch(err => {
+        console.log("failed to open")
+        console.log(err)
+    })
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
