3ebc936258840b58cfad65215634d2042a809ef4
KyleHS <k15156@yahoo.com>
2023-08-29 15:34:35 -0600

    Created a form to make new products and add to database

--- index.js
+++ index.js
@@ -16,10 +16,21 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}));
 
 app.get('/products', async (req, res) => { //very common pattern
     const products = await Product.find({})//how we match every product but does take time
     res.render('products/index', { products }) //not need to put .ejs
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct=  new Product(req.body);  //very simple version of this no limitations
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
 })
 
 app.get('/products/:id', async (req, res) => { 
