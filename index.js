8f898aa81eefdccf2268c962f2e875fd5a1c11b9
KyleHS <k15156@yahoo.com>
2023-08-29 17:45:58 -0600

    Createdd a way for user to edit the products

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-
+const methodOverride = require('method-override');
 const Product = require('./models/product'); //how we include from other js file
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true })
@@ -17,6 +17,9 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
+
+const categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/products', async (req, res) => { //very common pattern
     const products = await Product.find({})//how we match every product but does take time
@@ -24,7 +27,19 @@ app.get('/products', async (req, res) => { //very common pattern
 })
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', { categories })
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render('products/edit', { product, categorries })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+    res.redirect(`/products/${product._id}`)
 })
 
 app.post('/products', async (req, res) => {
